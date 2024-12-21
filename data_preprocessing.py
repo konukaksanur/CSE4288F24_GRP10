4045dbf9560eefa62bf125b7f0c0f7df609245b7
Zeynep Yılmaz <102660467+zenepyilmaz@users.noreply.github.com>
2024-12-22 01:40:44 +0300

    added new classifiers

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -26,6 +26,11 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import silhouette_score
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+from sklearn.naive_bayes import MultinomialNB  # Naive Bayes import
+from sklearn.tree import DecisionTreeClassifier #gain
+from sklearn.neural_network import MLPClassifier
+
+
 
 
 
@@ -227,7 +232,10 @@ def data_preprocessing(df):
     classifier = {
         "Logistic Regression": LogisticRegression(),
         "Random Forest Classsifier": RandomForestClassifier(),
-        "Support Vector Classifier" : SVC()
+        "Support Vector Classifier" : SVC(),
+        "Naive Bayes": MultinomialNB(),  
+        "Decision Tree (Gini)": DecisionTreeClassifier(criterion='gini'),
+        "Artificial Neural Network (ANN)": MLPClassifier(),
     }
 
     for name, classifier in classifier.items():
